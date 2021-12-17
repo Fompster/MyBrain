a94c73e7941670b8c0849a6ce7fa3a952e774f43
Fompster <fompsta@gmail.com>
2021-12-17 11:39:04 +0100

    Used classes in ball bounce

--- Scripts/Useful_Scripts/navBar.js
+++ Scripts/Useful_Scripts/navBar.js
@@ -4,11 +4,11 @@ const navBarHeight = $('.navBarBackground').offset().top + $('.navBarBackground'
 $(".navBarBackground").css("opacity", 0);
 
 $(window).on('scroll', function() {
+
   // we round here to reduce a little workload
   stop = Math.round($(window).scrollTop());
-  console.log(stop, headerBottom - navBarHeight, stop > headerBottom - navBarHeight, $(".navBarBackground").css("opacity") == 0)
-  if (($(".navBarBackground").css("opacity") == 0) && (stop > headerBottom - navBarHeight)) {
-    
+  if (($(".navBarBackground").css("opacity") == 0) && (stop > headerBottom - navBarHeight)) { 
+    $(".navBarBackground").css("background", $(".header").css("background")); 
     $(".navBarBackground").animate({opacity: 1}, 400);
   } else if (($(".navBarBackground").css("opacity") == 1) && (stop <= headerBottom - navBarHeight)) {
     $(".navBarBackground").animate({opacity: 0}, 200);
